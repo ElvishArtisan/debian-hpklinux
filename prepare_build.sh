14ef4286fb74917165f29647a820af53865b9434
Fred Gleason <fredg@paravelsystems.com>
2021-08-16 15:18:00 -0400

    2021-08-16 Fred Gleason <fredg@paravelsystems.com> 	* Modified 'prepare_build.sh' to automatically generate the 	correct 'linux-headers-<ver>' run time dependency.
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

--- prepare_build.sh
+++ prepare_build.sh
@@ -5,6 +5,8 @@ ASIHPI_VERSION=4.20.35
 rm -f *.buildinfo *.changes *.ddeb *.deb *.dsc *.debian.tar.xz
 rm -rf hpklinux_$ASIHPI_VERSION
 rm -rf hpklinux_$ASIHPI_VERSION-orig
+rm -f debian/control
 tar -zvxf hpklinux_$ASIHPI_VERSION.tar.gz
+sed s/@KERNEL_VERSION@/`uname -r`/ < debian/control.src > debian/control
 cp -a debian hpklinux_$ASIHPI_VERSION/
 tar -zvcf hpklinux_$ASIHPI_VERSION.orig.tar.gz hpklinux_$ASIHPI_VERSION
