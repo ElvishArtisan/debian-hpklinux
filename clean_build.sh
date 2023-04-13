6aaf0fc457dd0a2f462fa60218c5d979b6917641
Fred Gleason <fredg@paravelsystems.com>
2023-04-13 13:40:02 -0400

    2023-04-13 Fred Gleason <fredg@paravelsystems.com> 	* Added a 'clean_build.sh' script.
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

---
+++ clean_build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+ASIHPI_VERSION=4.20.35
+
+rm -f *.buildinfo *.changes *.ddeb *.deb *.dsc *.debian.tar.xz
+#rm -rf hpklinux_$ASIHPI_VERSION
+#rm -rf hpklinux_$ASIHPI_VERSION-orig
+rm -rf hpklinux_*
+rm -f debian/control
