574069a88256019600c01bf9c03dd8ab9398e946
Fred Gleason <fredg@paravelsystems.com>
2023-12-21 16:32:33 -0500

    2023-12-21 Fred Gleason <fredg@paravelsystems.com> 	* Added a check to ensure that the kernel module is built, installed 	and loaded after each system reboot.
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

--- clean_build.sh
+++ clean_build.sh
@@ -5,3 +5,4 @@ ASIHPI_VERSION=4.20.39
 rm -f *.buildinfo *.changes *.ddeb *.deb *.dsc *.debian.tar.xz *.orig.tar.gz
 rm -rf hpklinux_$ASIHPI_VERSION
 rm -f debian/control
+rm -f debian/load_asihpi.sh
--- prepare_build.sh
+++ prepare_build.sh
@@ -8,5 +8,7 @@ rm -rf hpklinux_$ASIHPI_VERSION-orig
 rm -f debian/control
 tar -zvxf hpklinux_$ASIHPI_VERSION.tar.gz
 sed s/@KERNEL_VERSION@/`uname -r`/ < debian/control.src > debian/control
+sed s/@ASIHPI_VERSION@/$ASIHPI_VERSION/ < debian/load_asihpi.sh.in > debian/load_asihpi.sh
+chmod 755 debian/load_asihpi.sh
 cp -a debian hpklinux_$ASIHPI_VERSION/
 tar -zvcf hpklinux_$ASIHPI_VERSION.orig.tar.gz hpklinux_$ASIHPI_VERSION
