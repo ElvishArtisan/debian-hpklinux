a067389993231dd2e40145d48671adf786e7733f
Fred Gleason <fredg@paravelsystems.com>
2023-12-22 10:16:35 -0500

    2023-12-22 Fred Gleason <fredg@paravelsystems.com> 	* Incremented the package version to 4.20.39-2.
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

--- versions.sh
+++ versions.sh
@@ -4,6 +4,6 @@
 # When incrementing versions, be sure to create a new entry in
 # 'debian/changelog'!
 #
-ASIHPI_PKG_VERSION=4.20.39-1
+ASIHPI_PKG_VERSION=4.20.39-2
 ASIHPI_VERSION=4.20.39
 ASIHPI_MAJOR_VERSION=4.20
