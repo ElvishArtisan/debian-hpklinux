b9a59feba88b22ebbafafba2501ab054b53af495
Fred Gleason <fredg@paravelsystems.com>
2024-05-03 15:37:45 -0400

    Changed to placeholder branch
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

--- versions.sh
+++
@@ -1,9 +0,0 @@
-#
-# Maintainer's Note:
-#
-# When incrementing versions, be sure to create a new entry in
-# 'debian/changelog'!
-#
-ASIHPI_PKG_VERSION=4.20.39-3
-ASIHPI_VERSION=4.20.39
-ASIHPI_MAJOR_VERSION=4.20
